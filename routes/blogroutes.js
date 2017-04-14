c1e467eca92c62e60e3e9e03ea8c2f2cf42ba4f9
Bill Boughton <boughton.william@gmail.com>
2017-04-14 11:20:49 -0600

    all routes working will adjust to get one to pull from both tables then heroku

--- routes/blogroutes.js
+++ routes/blogroutes.js
@@ -11,4 +11,66 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+  var blogId = req.params.id
+  knex('blogs').where('id', blogId)
+    .then(result => {
+      res.send(result)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.post('/new', (req, res) =>{
+  var newAuthor = req.body.author
+  var newBody = req.body.body
+  var newURL = req.body.image_url
+  var newTitle = req.body.title
+  knex('blogs').insert({author:newAuthor, body:newBody, title:newTitle, image_url:newURL, votes:0}).returning(['id', 'author', 'body', 'title', 'votes', 'image_url', 'date'])
+    .then(result => {
+      res.send(result)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.patch('/update/:id', (req, res) => {
+  var changeId = req.params.id
+  var changeBody = req.body.body
+  var changeTitle = req.body.title
+  var changeURL = req.body.image_url
+  knex('blogs').where('id', changeId).update({title:changeTitle, body:changeBody, image_url:changeURL})
+    .then(result => {
+      res.send(200)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.patch('/votes/:id', (req, res) => {
+  var votesId = req.params.id
+  var changeVotes = req.body.votes
+  knex('blogs').where('id', votesId).update({votes:changeVotes})
+    .then(result => {
+      res.send(200)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  var dId = req.params.id
+  knex('blogs').where('id', dId).del()
+    .then(result => {
+      res.send(200)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
 module.exports = router
--- routes/commentroutes.js
+++ routes/commentroutes.js
@@ -1,11 +1,59 @@
 const router = require('express').Router()
 const knex =  require('../db/knex.js')
 
-
-router.get('/', (req, res) => {
-  knex('comments')
+router.get('/:id', (req, res) => {
+  var getComm = req.params.id
+  knex('comments').where('id', getComm)
     .then(result => {
       res.send(result)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.get('/allperblog/:id', (req, res) => {
+  var blogId = req.params.id
+  knex('comments').where('blog_id', blogId)
+    .then(result => {
+      res.send(result)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.post('/new', (req, res) => {
+  var newAuthor = req.body.author
+  var newComment = req.body.comment
+  var blogId = req.body.blog_id
+  knex('comments').insert({author:newAuthor, comment:newComment, blog_id:blogId}).returning(['id', 'author', 'blog_id', 'comment'])
+    .then(result => {
+      res.send(result)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.patch('/update/:id', (req, res) => {
+  var pId = req.params.id
+  var changeComment = req.body.comment
+  var changeAuthor = req.body.author
+  knex('comments').where('id', pId).update({comment:changeComment, author:changeAuthor})
+    .then(result => {
+      res.send(200)
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  var dId = req.params.id
+  knex('comments').where('id', dId).del()
+    .then(result => {
+      res.send(200)
     })
     .catch(error => {
       res.send(error)
